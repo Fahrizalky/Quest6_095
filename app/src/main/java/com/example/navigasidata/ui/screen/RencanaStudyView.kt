6438b4a48ec18cf6e3cc11b296eaa4c1c0ea8eeb
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-11-22 14:33:41 +0700

    menambahkan Image pada var Listdata

--- app/src/main/java/com/example/navigasidata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasidata/ui/screen/RencanaStudyView.kt
@@ -1,15 +1,21 @@
 package com.example.navigasidata.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.navigasidata.R
 import com.example.navigasidata.model.Mahasiswa
 import com.example.navigasidata.model.RencanaStudy
@@ -38,6 +44,14 @@ fun RencanaStudyView(
             .fillMaxSize()
             .background(color =  colorResource(id = R.color.primary))
     ){
+
+        Image(
+            painter = painterResource(id = R.drawable.umy),
+            contentDescription =  "",
+            modifier = Modifier
+                .clip(shape = CircleShape)
+                .size(50.dp)
+        )
 
     }
 
